bd12024b4549b9ef4bb72ad4e1e3479d38ff3057
Marcelo Lopes <lopes.cte@gmail.com>
2024-05-27 23:36:44 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; mirror $INPUT_OPTIONS --reverse --continue --dereference -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; mirror --reverse --continue --dereference $INPUT_OPTIONS -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
 composer update
 vendor/bin/phinx migrate
